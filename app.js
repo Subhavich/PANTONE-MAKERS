343479dd1972f4ffe59d66c8e0e58e796b64a35a
Subhavich <subhavichads@gmail.com>
2024-01-09 05:49:26 +0700

    HTML,JS -> add live input text logic

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ const yChanger = document.querySelector("#y-percent");
 const sizeChanger = document.querySelector("#scale-percent");
 const bgPosChanger = document.querySelector("#bgPosition");
 const bgChanger = document.querySelector("form");
+const line1 = document.querySelector("#line-1");
+const line2 = document.querySelector("#line-2");
+const line3 = document.querySelector("#line-3");
+const cardTitle = document.querySelector("span");
+const line1Input = document.querySelector("#line-1-input");
+const line2Input = document.querySelector("#line-2-input");
+const line3Input = document.querySelector("#line-3-input");
+const cardTitleInput = document.querySelector("#hero-input");
+
 // change pic src to form url
 bgChanger.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -33,4 +42,22 @@ sizeChanger.addEventListener("input", (e) => {
 bgPosChanger.addEventListener("change", (e) => {
   const newPos = e.target.value;
   imageContainer.style.backgroundPosition = newPos;
+});
+
+// change text to be like current input
+// 1
+line1Input.addEventListener("input", (e) => {
+  line1.textContent = e.target.value;
+});
+// 2
+line2Input.addEventListener("input", (e) => {
+  line2.textContent = e.target.value;
+});
+// 3
+line3Input.addEventListener("input", (e) => {
+  line3.textContent = e.target.value;
+});
+// card title
+cardTitleInput.addEventListener("input", (e) => {
+  cardTitle.textContent = e.target.value;
 });
